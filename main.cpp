c757f784b47af89e9f95896da528e1edd16f0cff
asanvlit <asanvlit@gmail.com>
2022-11-27 21:04:08 +0300

    parallel gauss

--- main.cpp
+++ main.cpp
@@ -18,18 +18,15 @@ int* pPivotIter;
 int* pRowStartIndex; // Номер строки, с которой начинается блок для данного процесса
 int* pRowNumber;     // Количество строк для данного процесса
 
-void printMatrix(double* m, int rowCount, int colCount) {
-    for (int i = 0; i < rowCount; i++) {
-        for (int j = 0; j < colCount; j++) {
-            printf("%7.4f ", m[i * colCount + j]);
-        }
-        printf("\n");
-    }
-}
-
-void printVector(double* v, int n) {
+void printSystem(double* m, double* v, int n) {
     for (int i = 0; i < n; i++) {
-        printf("%7.4f ", v[i]);
+        for (int j = 0; j < n; j++) {
+            printf("%7.4f*x%i", m[i * n + j], j);
+            if (j < n - 1) {
+                printf(" + ");
+            }
+        }
+        printf(" = %f\n", v[i]);
     }
 }
 
@@ -39,7 +36,7 @@ void printResultVector(double* resultV, int n) {
     }
 }
 
-void fillUpMatrixWithRandom(double* pMatrix, double* pVector, int n) {
+void fillUpSystemWithRandom(double* pMatrix, double* pVector, int n) {
     srand(unsigned(clock()));
 
     for (int i = 0; i < n; i++) {
@@ -65,7 +62,7 @@ void initProcess(double*& a, double*& v, double*& result, double*& pA, double*& 
         a = new double[n * n];
         v = new double[n];
         result = new double[n];
-        fillUpMatrixWithRandom(a, v, n);
+        fillUpSystemWithRandom(a, v, n);
     }
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
@@ -82,18 +79,18 @@ void initProcess(double*& a, double*& v, double*& result, double*& pA, double*& 
     pParallelPivotPos = new int[n];
     pPivotIter = new int[pBlockSize];
 
-    pRowStartIndex = new int[size];
-    pRowNumber = new int[size];
-
     for (int i = 0; i < pBlockSize; i++) {
         pPivotIter[i] = -1; // пока никакие строки в качестве ведущих не использовались
     }
+
+    pRowStartIndex = new int[size];
+    pRowNumber = new int[size];
 }
 
 // Распределение данных между процессами
-void dataDistribution(double* pMatrix, double* pProcRows, double* pVector, double* pProcVector, int n, int blockSize) {
+void distributeSystem(double* a, double* pRows, double* v, double* pVector, int n, int blockSize) {
     int* pSendNum;	     // Количество элементов, отправленных на процесс
-    int* pSendInd;	     // Номер первого элемента, отпарвленного на процесс
+    int* pSendInd;	     // Номер первого элемента, отправленного на процесс
     int remRowsNumb = n; // Количество строк, которые еще не были распределены
 
     pSendInd = new int[size];
@@ -111,7 +108,8 @@ void dataDistribution(double* pMatrix, double* pProcRows, double* pVector, doubl
     }
 
     // Рассылка строк
-    MPI_Scatterv(pMatrix, pSendNum, pSendInd, MPI_DOUBLE, pProcRows, pSendNum[rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Scatterv(a, pSendNum, pSendInd, MPI_DOUBLE, pRows,
+                 pSendNum[rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     // Определение позиций строк матрицы для текущего процесса:
     remRowsNumb = n;
@@ -123,7 +121,8 @@ void dataDistribution(double* pMatrix, double* pProcRows, double* pVector, doubl
         pRowStartIndex[i] = pRowStartIndex[i - 1] + pRowNumber[i - 1];
     }
 
-    MPI_Scatterv(pVector, pRowNumber, pRowStartIndex, MPI_DOUBLE, pProcVector, pRowNumber[rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Scatterv(v, pRowNumber, pRowStartIndex, MPI_DOUBLE, pVector,
+                 pRowNumber[rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     delete[] pSendNum;
     delete[] pSendInd;
@@ -134,44 +133,27 @@ void buildResult(double* pProcResult, double* pResult) {
                 pRowStartIndex, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 }
 
-void parallelEliminateColumns(double* pProcRows, double* pProcVector, double* pPivotRow, int n, int pBlockSize, int iter) {
-    double multiplier;
-
-    // вычитание ведущей строки из строк процесса, которые еще не использовались в качестве ведущих
-    for (int i = 0; i < pBlockSize; i++) {
-        if (pPivotIter[i] == -1) {
-            multiplier = pProcRows[i * n + iter] / pPivotRow[iter];
-
-            for (int j = iter; j < n; j++) {
-                pProcRows[i * n + j] -= pPivotRow[j] * multiplier;
-            }
-
-            pProcVector[i] -= pPivotRow[n] * multiplier;
-        }
-    }
-}
-
 // Прямой ход
-void parallelGaussianElimination(double* pProcRows, double* pProcVector, int n, int RowNum) {
-    int	PivotPos; // Позиция ведущего элемента
+void forwardGaussStep(double* pRows, double* pVector, int n, int pBlockSize) {
+    int	pivotPos; // Позиция ведущего элемента
 
     // Вспомогательная структура для выбора ведущей строки
     struct { double MaxValue; int ProcRank; } ProcPivot, Pivot;
 
-    // Ведущая строка и соответсвующий элемент вектора (после = )
+    // Ведущая строка и соответсвующий элемент вектора
     double* pPivotRow = new double[n + 1];
 
     // Итерации
     for (int i = 0; i < n; i++) {
         // Поиск ведущей строки
-        double MaxValue = 0; // Значение ведущего элемента
-        for (int j = 0; j < RowNum; j++) {
-            if ((pPivotIter[j] == -1) && (MaxValue < fabs(pProcRows[j * n + i]))) {
-                MaxValue = fabs(pProcRows[j * n + i]);
-                PivotPos = j;
+        double maxValue = 0; // Значение ведущего элемента
+        for (int j = 0; j < pBlockSize; j++) {
+            if ((pPivotIter[j] == -1) && (maxValue < fabs(pRows[j * n + i]))) {
+                maxValue = fabs(pRows[j * n + i]);
+                pivotPos = j;
             }
         }
-        ProcPivot.MaxValue = MaxValue;
+        ProcPivot.MaxValue = maxValue;
         ProcPivot.ProcRank = rank;
 
         // Поиск "ведущего" процесса - процесса с максимальным значением maxValue
@@ -180,82 +162,98 @@ void parallelGaussianElimination(double* pProcRows, double* pProcVector, int n, 
 
         // Рассылка ведущей строки
         if (rank == Pivot.ProcRank) {
-            pPivotIter[PivotPos] = i; // номер итерации
-            pParallelPivotPos[i] = pRowStartIndex[rank] + PivotPos;
+            pPivotIter[pivotPos] = i; // номер итерации
+            pParallelPivotPos[i] = pRowStartIndex[rank] + pivotPos;
         }
         MPI_Bcast(&pParallelPivotPos[i], 1, MPI_INT, Pivot.ProcRank,MPI_COMM_WORLD);
 
         if (rank == Pivot.ProcRank) {
             // Заполнение ведущей строки
             for (int j = 0; j < n; j++) {
-                pPivotRow[j] = pProcRows[PivotPos * n + j];
+                pPivotRow[j] = pRows[pivotPos * n + j];
             }
-            pPivotRow[n] = pProcVector[PivotPos];
+            pPivotRow[n] = pVector[pivotPos];
         }
         MPI_Bcast(pPivotRow, n + 1, MPI_DOUBLE, Pivot.ProcRank, MPI_COMM_WORLD);
 
-        parallelEliminateColumns(pProcRows, pProcVector, pPivotRow, n, RowNum, i);
+        double multiplier;
+        // вычитание ведущей строки из строк процесса, которые еще не использовались в качестве ведущих
+        for (int x = 0; x < pBlockSize; x++) {
+            if (pPivotIter[x] == -1) {
+                multiplier = pRows[x * n + i] / pPivotRow[i];
+
+                for (int j = i; j < n; j++) {
+                    pRows[x * n + j] -= pPivotRow[j] * multiplier;
+                }
+
+                pVector[x] -= pPivotRow[n] * multiplier;
+            }
+        }
     }
 }
 
-// Function for finding the pivot row of the back substitution
-void findBackPivotRow(int RowIndex, int n, int& IterProcRank, int& IterPivotPos) {
+// Функция для нахождения ведущей строки обратной подстановки
+void findBackPivotRow(int rowIndex, int& iterProcRank, int& iterPivotPos) {
     for (int i = 0; i < size - 1; i++) {
-        if ((pRowStartIndex[i] <= RowIndex) && (RowIndex < pRowStartIndex[i + 1])) IterProcRank = i;
+        if ((pRowStartIndex[i] <= rowIndex) && (rowIndex < pRowStartIndex[i + 1])) {
+            iterProcRank = i;
+        }
     }
-    if (RowIndex >= pRowStartIndex[size - 1]) IterProcRank = size - 1;
-    IterPivotPos = RowIndex - pRowStartIndex[IterProcRank];
+    if (rowIndex >= pRowStartIndex[size - 1]) {
+        iterProcRank = size - 1;
+    }
+
+    iterPivotPos = rowIndex - pRowStartIndex[iterProcRank];
 }
 
 // Обратный ход:
-void parallelBackSubstitution(double* pProcRows, double* pProcVector, double* pProcResult, int n, int RowNum) {
+void backwardGaussStep(double* pRows, double* pVector, double* pResult, int n, int pBlockSize) {
     int iterProcRank;	// Номер процесса с данной ведущей строкой
     int iterPivotPos;	// Позиция ведущей строки в процессе
     double iterResult;	// Вычисленное значение неизвестной
-    double val;
+    double value;
 
     // Итерации
     for (int i = n - 1; i >= 0; i--) {
         // Определение ранга процесса, который содержит ведущую строку
-        findBackPivotRow(pParallelPivotPos[i], n, iterProcRank, iterPivotPos);
+        findBackPivotRow(pParallelPivotPos[i], iterProcRank, iterPivotPos);
 
         // Вычисление неизвестной
         if (rank == iterProcRank) {
-            iterResult = pProcVector[iterPivotPos] / pProcRows[iterPivotPos * n + i]; pProcResult[iterPivotPos] = iterResult;
+            iterResult = pVector[iterPivotPos] / pRows[iterPivotPos * n + i];
+            pResult[iterPivotPos] = iterResult;
         }
         // Рассылка значения вычисленной неизвестной
         MPI_Bcast(&iterResult, 1, MPI_DOUBLE, iterProcRank, MPI_COMM_WORLD);
 
         // Обновление значений вектора
-        for (int j = 0; j < RowNum; j++) {
+        for (int j = 0; j < pBlockSize; j++) {
             if (pPivotIter[j] < i) {
-                val = pProcRows[j * n + i] * iterResult;
-                pProcVector[j] = pProcVector[j] - val;
+                value = pRows[j * n + i] * iterResult;
+                pVector[j] = pVector[j] - value;
             }
         }
     }
 }
 
-void printInitialData(double* pMatrix, double* pVector, double* pProcRows, double* pProcVector, int n, int RowNum) {
+void printInitialData(double* pMatrix, double* pVector, double* pProcRows, double* pProcVector, int n, int pBlockSize) {
     if (rank == 0) {
-        printf("Initial Matrix: \n");
-        printMatrix(pMatrix, n, n);
-        printf("Initial Vector: \n");
-        printVector(pVector, n);
+        printf("Initial data: \n");
+        printSystem(pMatrix, pVector, n);
     }
     MPI_Barrier(MPI_COMM_WORLD);
 }
 
 void printResultData(double* result, int n) {
     if (rank == 0) {
-        printf("\n Result Vector: \n");
+        printf("\nResult Vector: \n");
         printResultVector(result, n);
     }
 }
 
-void gauss(double* pProcRows, double* pProcVector, double* pProcResult, int n, int RowNum) {
-    parallelGaussianElimination(pProcRows, pProcVector, n, RowNum);
-    parallelBackSubstitution(pProcRows, pProcVector, pProcResult, n, RowNum);
+void gauss(double* pRows, double* pVector, double* pResult, int n, int rowNum) {
+    forwardGaussStep(pRows, pVector, n, rowNum);
+    backwardGaussStep(pRows, pVector, pResult, n, rowNum);
 }
 
 // Проверка результата
@@ -280,10 +278,12 @@ void checkResult(double* pMatrix, double* pVector, double* pResult, int n) {
                 isEqual = 1;
             }
         }
-        if (isEqual == 1)
-            printf("The result of the parallel Gauss algorithm is NOT correct." "Check your code.");
-        else
-            printf("The result of the parallel Gauss algorithm is correct.");
+        if (isEqual == 1) {
+            printf("\nThe result of the Gauss algorithm is NOT correct");
+        }
+        else {
+            printf("\nThe result of the Gauss algorithm is correct");
+        }
         delete[] pRightPartVector;
     }
 }
@@ -308,38 +308,37 @@ void freeMemory(double* pMatrix, double* pVector, double* pResult, double* pProc
 }
 
 int main(int argc, char* argv[]) {
-    double* a;            // Матрица
-    double* v;            // Вектор
-    double* result;       // Вычисленные неизвестные
-    double* pA;           // Строки матрицы для данного процесса
-    double* pV;	          // Кусок вектора для данного процесса
-    double* pResult;  // Block of the v x
     int	n;
-    int	rowNum;	// Number of the matrix rows
-    double start, finish, duration;
+    int	pBlockSize;	  // Количество строк матрицы в блоке
+
+    double* a;        // Матрица
+    double* v;        // Вектор
+    double* result;   // Вычисленные неизвестные
+    double* pA;       // Строки матрицы для данного процесса
+    double* pV;	      // Кусок вектора для данного процесса
+    double* pResult;  // Block of the v x
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     setvbuf(stdout, 0, _IONBF, 0);
-    initProcess(a, v, result, pA, pV, pResult, n, rowNum);
+    initProcess(a, v, result, pA, pV, pResult, n, pBlockSize);
 
-    start = MPI_Wtime();
-    dataDistribution(a, pA, v, pV, n, rowNum);
-    gauss(pA, pV, pResult, n, rowNum);
-    printInitialData(a, v, pA, pV, n, rowNum);
+    double start = MPI_Wtime();
+    distributeSystem(a, pA, v, pV, n, pBlockSize);
+    gauss(pA, pV, pResult, n, pBlockSize);
+    printInitialData(a, v, pA, pV, n, pBlockSize);
     buildResult(pResult, result);
-    finish = MPI_Wtime();
+    double finish = MPI_Wtime();
 
-    duration = finish - start;
-
-    printResultData(result, n);
-    checkResult(a, v, result, n);
-
+    double duration = finish - start;
     if (rank == 0) {
         printf("\n Time of execution: %f\n", duration);
     }
+
+    printResultData(result, n);
+    checkResult(a, v, result, n);
 
     freeMemory(a, v, result, pA, pV, pResult);
 
